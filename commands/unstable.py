79969a98a0059d575b958ce206bd31c58c57a0ab
bltzxsd <blissardartinq@gmail.com>
2022-09-17 02:01:33 +0530

    add unstable evaluate

--- commands/unstable.py
+++ commands/unstable.py
@@ -2,6 +2,8 @@ import asyncio
 import operator
 
 import interactions
+import py_expression_eval
+import pyfiglet
 import tabulate
 from interactions import CommandContext
 from lib import constants, misc
@@ -125,6 +127,45 @@ class Unstable(interactions.Extension):
         except asyncio.TimeoutError:
             misc.disable(buttons)
             return await ctx.edit(components=interactions.spread_to_rows(*buttons))
+
+    @unstable.subcommand(
+        name="evaluate",
+        description="Evaluates compound dice rolls. ",
+        options=[
+            interactions.Option(
+                name="expr",
+                description="Dice expression. Eg: `1d8+4+1d6+1d6+1d6",
+                type=interactions.OptionType.STRING,
+                required=True,
+            ),
+            interactions.Option(
+                name="ephemeral",
+                description="Whether the result should only be visible to the user.",
+                type=interactions.OptionType.BOOLEAN,
+                required=False,
+            ),
+        ],
+    )
+    async def complex(
+        self, ctx: interactions.CommandContext, expr: str, ephemeral: bool = False
+    ):
+        dice_syntax = expr
+        expr = misc.parse_compound_dice(expr)
+        parser = py_expression_eval.Parser()
+        try:
+            expression = parser.parse(expr).evaluate({})
+        except Exception as e:
+            title, desc, status = "Error", f"Exception Occured:\n{e}", "error"
+            ephemeral = True
+        else:
+            figlet = pyfiglet.figlet_format(str(expression), "fraktur")
+            figlet = figlet.replace("`", "\u200B`")
+            desc = f"```{figlet}```" if len(figlet) < 1024 else f"**{expression}**"
+            title, status = f"Evaluation: {dice_syntax}", "ok"
+
+        embed = misc.quick_embed(title, desc, status)
+        embed.set_footer(f"{expr} = {expression}")
+        await ctx.send(embeds=embed, ephemeral=ephemeral)
 
 
 def setup(client):
